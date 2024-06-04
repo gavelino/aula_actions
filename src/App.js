c12b40caff915e0a2d38fa36aa2bebcf7cc80177
Guilherme Avelino <gavelino@gmail.com>
2024-06-04 09:05:35 -0300

    test master CI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub Actions 
+          Aula GitHub Actions - test Master integration
         </p>
         <a
           className="App-link"

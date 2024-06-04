4c728957d71240bb61a52888e0969e950998d2a4
Guilherme Avelino <gavelino@gmail.com>
2024-06-04 09:24:13 -0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {} 
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub Actions - New Workflow test
+          Aula GitHub Actions - test pull request CI
         </p>
         <a
           className="App-link"
